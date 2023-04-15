34bf16f0c3f4b2d20d9c0a8d0bcc5f5fe9a64864
jonni2 <jonnijonni200@gmail.com>
2023-04-15 15:44:26 +0200

    Modified Root Analysis

--- Analysis.C
+++ Analysis.C
@@ -3,22 +3,22 @@
 // function to plot a CSV file
 void Plot(TString path) {
     
-    TGraph* g1 = new TGraph(path, "%lg %*lg %*lg %lg", ",");
+    TGraph* g1 = new TGraph(path, "%lg %lg", ",");
     
-    g1->SetTitle("High Voltage scan, PMT 7, 8, using 9, 10 as REFs; PMTs Voltage (V); Counts");
-    g1->SetMarkerStyle(6);
+    g1->SetTitle("High Voltage scan, PMT 7-8; PMTs Voltage (V); Counts");
+    g1->SetMarkerStyle(7);
     g1->SetMarkerColor(kBlue);
     g1->SetLineColor(kBlue);
     
-    TGraph* g2 = new TGraph(path, "%lg %*lg %*lg %*lg %lg", ",");
-    g2->SetMarkerStyle(6);
+    TGraph* g2 = new TGraph(path, "%lg %*lg %lg", ",");
+    g2->SetMarkerStyle(7);
     g2->SetMarkerSize(2);
     g2->SetMarkerColor(kRed);
     g2->SetLineColor(kRed);
     
     TLegend* leg = new TLegend(.1, .7, .4, .9);
-    leg->AddEntry(g1, "PMT 7 coinc. with 9, 10");
-    leg->AddEntry(g2, "PMT 8 coinc. with 9, 10");
+    leg->AddEntry(g1, "PMT 7 single counts");
+    leg->AddEntry(g2, "PMT 8 single counts");
     
     TCanvas* c = new TCanvas("c", "HV scan");
     c->cd();
