45daf3af40942abee20fa4b6ca86ee018709b8e4
jonni2 <jonnijonni200@gmail.com>
2023-05-27 17:07:20 +0200

    Random changes that don't work

--- AnalysisRoofit.cpp
+++ AnalysisRoofit.cpp
@@ -11,19 +11,21 @@ void AnalysisRoofit(){
     RooConstVar R("R", "R", 1.21);
     RooFormulaVar R1("R1", "1/R", "1/R", RooArgSet(R));
     
-    //RooRealVar b("b", "b", 10, 0, 50);
-    RooUniform b("b", "b", t);
+    // RooRealVar b("b", "b", 10, 0, 50);
+    RooUniform bkg("b", "b", t);
+    // RooPolynomial bkg("bkg", "bkg", b);
 
     RooExponential e0("e0", "e0", t, rate0);
     RooExponential e_mu("e_mu", "e_mu", t, rate_mu);
-    RooProdPdf e_muR("e_muR", "e_muR", RooArgList(e_mu, R1)); 
+    // RooProdPdf e_muR("e_muR", "e_muR", RooArgList(e_mu, R1)); 
 
-    RooRealVar frac("frac", "Fraction", 0.5, 0, 1);
-    RooAddPdf model0("model0", "model0", RooArgList(e0, e_muR), RooArgList(frac));
+    // RooConstVar frac("frac", "Fraction", 0.5, 0, 1);
+    RooAddPdf model0("model0", "model0", RooArgList(e0, e_mu), RooArgList(R1));
     
-    RooRealVar frac_b("frac_b", "frac_b", 0.5, 0, 1);
 
-    RooAddPdf model("model", "model", RooArgList(b, model0), RooArgList(frac_b));
+    RooRealVar nsig("nsig", "nsig", 1000, 0, 10000);
+    RooRealVar nbkg("nsig", "nsig", 10, 0, 100);
+    RooAddPdf model("model", "model", RooArgList(bkg, model0), RooArgList(nbkg, nsig));
 
     //load unbinned dataset
     RooDataSet* data = RooDataSet::read("Data/Muon_Data/R3_data_P1_ns.csv", t);
@@ -40,10 +42,9 @@ void AnalysisRoofit(){
 
 
     //plot
-
+    model.fitTo(*data);
     data->plotOn(frame);
     model.plotOn(frame); // always before fitTo
-    model.fitTo(*dataRange);
     frame->Draw();
 
 
