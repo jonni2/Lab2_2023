0b1aba4338aa824ebc73c2bd0565845a8b931faf
jonni2 <jonnijonni200@gmail.com>
2023-06-08 15:44:20 +0200

    Corrected pdf

--- AnalysisRoofit.cpp
+++ AnalysisRoofit.cpp
@@ -34,58 +34,40 @@ void AnalysisRoofit(){
     RooFormulaVar rate0("rate0", "rate", "-1/tau0", RooArgSet(tau0));
     RooRealVar tau_mu("tau_mu", "#tau_{#mu}", 200, 100, 500);
     RooFormulaVar rate_mu("rate_mu", "rate", "-1/tau_mu", RooArgSet(tau_mu));
-    // RooConstVar R("R", "R", 1.21);
-    // RooFormulaVar R1("R1", "1/R", "1/R", RooArgSet(R));
     
-    // RooRealVar b("b", "b", 1, 0, 50);
+    RooConstVar R("R", "R", 1.21);
+    RooFormulaVar R1("R1", "1/R", "1/R", RooArgSet(R));
+    
+    RooFormulaVar exp("exp", "e^(-t/tau0)+R1*e^(-t/tau_mu)", RooArgList(t, tau0, R1, tau_mu));
+    
     RooUniform bkg("bkg", "bkg", t);
-    // RooPolynomial bkg("bkg", "bkg", b);
 
-    RooExponential e0("e0", "e0", t, rate0);
-    RooExponential e_mu("e_mu", "e_mu", t, rate_mu);
-    // RooProdPdf e_muR("e_muR", "e_muR", RooArgList(e_mu, R1)); 
-
-    // RooFormulaVar e_muR("model0", "@0*@1", RooArgList(R1, e_mu));
-    
-    // RooRealVar f0("f0", "Fraction of e0", 0.5, 0, 1);
-    // RooRealVar fmu("fmu", "Fraction of e_mu", 0.5, 0, 1);
-    // RooGenericPdf model0("model0", "model0", "@0+@1", RooArgList(e_muR, e0));
-    
 
     // Using Number of EVENTS
-    RooRealVar n0("n0", "n0", 1000, 0, 16000);
-    RooRealVar n_mu("n_mu", "n_mu", 800, 0, 16000);
-    RooRealVar nbkg("nbkg", "nbkg", 100, 0, 16000);
+    // RooRealVar nsig("nsig", "nsig", 1000, 0, 16000);
+    // RooRealVar nbkg("nbkg", "nbkg", 100, 0, 16000);
     
     // Using FRACTIONS
-    // RooRealVar f0("f0", "f0", 0.8, 0, 1);
-    // RooRealVar f_mu("f_mu", "f_mu", 0.2, 0, 1);
+    RooRealVar fsig("nsig", "nsig", 0.1000, 0, 1);
     
-    RooAddPdf model("model", "model", RooArgList(e0, e_mu, bkg), RooArgList(n0, n_mu, nbkg));
-    // RooAddPdf model("model", "model", RooArgList(model0, bkg), RooArgList(nsig, nbkg));
-
+    // Signal pdf = sum of the two exponentials
+    RooGenericPdf sig("sig", "sig", "@0", RooArgList(exp));
+    RooAddPdf model("model", "model", RooArgList(sig, bkg), RooArgList(fsig));
+    
     //load unbinned dataset
-    RooDataSet data = *RooDataSet::read("Data/Muon_Data/R3_data_P1_ns.csv", t);
+    RooDataSet data = *RooDataSet::read("Data/Muon_Data/R3_data_P3_ns.csv", t);
 
-
-    // double tmin = 20;
-    // double tmax = 10000;
-    //RooConstVar tmin ("tmin", "tmin", 20);
-    //RooConstVar tmax ("tmax", "tmax", 10000);
-    // RooDataSet* dataRange = (RooDataSet*) data->reduce(RooFit::Range(tmin, tmax));
-
-
+    // Frame for plotting
     RooPlot* frame = t.frame(RooFit::Title("Muon decay times"));
 
+    // Fitting model to data
+    model.fitTo(data, RooFit::Range(200, 16000));
 
-    //plot
-    model.fitTo(data);
+    // Plot data and model on the same frame
     data.plotOn(frame);
     model.plotOn(frame); // always before fitTo
-    // e0.plotOn(frame,RooFit::LineColor(kRed));
-    // e_mu.plotOn(frame,RooFit::LineColor(kOrange));
-    // bkg.plotOn(frame, RooFit::LineColor(kBlack));
     frame->Draw();
+    
     
     // Creating the LIKELIHOOD
     RooAbsReal* nll = model.createNLL(data);
@@ -96,8 +78,5 @@ void AnalysisRoofit(){
     frame_contour->SetTitle("Contour plot, #tau_{0} vs #tau_{#mu}");
     frame_contour->Draw();
     
-    // RooPlot* frame_tau = tau0.frame();
-    // nll->plotOn(frame_tau);
-    // frame_tau->Draw();
 
 }
